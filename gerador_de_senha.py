d65528a6ea62152e59f96f9b116769022c3a5e38
GugaValenca <gustavo_valenca@hotmail.com>
2025-04-23 18:45:28 -0500

    add implementation of password generation function in gerador_de_senha.py

---
+++ gerador_de_senha.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def gerar_senha(tamanho):
+    # Tua lógica aqui pra misturar letras, números e símbolos
+    pass
+
+# Teste
+print(gerar_senha(12))  # Senha aleatória com 12 caracteres
