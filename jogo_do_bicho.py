fbea314d1e9bac2aa7c7d6e3834a35a752ef43ae
GugaValenca <gustavo_valenca@hotmail.com>
2025-05-04 14:30:31 -0500

    add jogo_do_bicho.py for animal guessing game functionality

---
+++ jogo_do_bicho.py
@@ -0,0 +1,14 @@
+numero = int(input("Digite um número de 1 a 25: "))
+
+if 1 <= numero <= 5:
+    print("Oxe! É uma Ave, homi!")
+elif 6 <= numero <= 10:
+    print("Rapaaaiz, deu Felino!")
+elif 11 <= numero <= 15:
+    print("Vixe Maria, saiu Réptil!")
+elif 16 <= numero <= 20:
+    print("Ôxi, apareceu um Inseto!")
+elif 21 <= numero <= 25:
+    print("Êita, deu Mamífero!")
+else:
+    print("Num seja doido não! Digite só número de 1 a 25, visse?")
